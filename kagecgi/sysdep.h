b461239ba2e6f21f30832d99f11426e90ef51c91
Koichi KAMICHI <kamichi@fonts.jp>
2004-02-27 15:13:43 +0000

    New file.

---
+++ kagecgi/sysdep.h
@@ -0,0 +1,20 @@
+//sysdep.h
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+#include <sys/fcntl.h>
+
+#include <glib.h>
+
+#include <png.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+#include <db1/db.h>
+//#include <db2/db_185.h>
+
