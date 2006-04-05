ee244e2163a6c301882711f4c9ff40e54ccdbee3
Koichi KAMICHI <kamichi@fonts.jp>
2006-04-05 13:37:45 +0000

    Unify chise_tools.i into chise.i. Expand buffer size for feature names.

--- tools/chiseperl.pl
+++ tools/chiseperl.pl
@@ -1,6 +1,6 @@
 # chiseperl.pl --- some useful funcions for using libchise with SWIG
 #
-# Copyright (C) 2005 Koichi Kamichi.
+# Copyright (C) 2005,2006 KAMICHI Koichi.
 #
 # global : $chise_ds, @chise_feature, @chise_ft, $chise_newid
 # function : void init_chise();
@@ -12,16 +12,16 @@
 #            $var create_new_char($feature, $value);
 #            $char_id decode_char($feature_char, $int);
 
-use chise_tools;
 use chise;
 
 sub init_chise{
+#$chise_ds = chise::CHISE_DS_open($chise::CHISE_DS_BERKELEY_DB, $chise::chise_system_db_dir, 0, 0755);
   $chise_ds = chise::CHISE_DS_open(0, $chise::chise_system_db_dir, 0, 0755);
-  chise_tools::listup_feature($chise_ds);
-  @chise_feature = split(/\n/, chise_tools::get_char($chise_tools::feature));
+  chise::listup_feature($chise_ds);
+  @chise_feature = split(/\n/, chise::get_char($chise::feature));
   @chise_ft = ();
   foreach(@chise_feature){
-    push(@chise_ft, chise::chise_ds_get_feature($chise_ds, chise_tools::get_uchar($_)));
+    push(@chise_ft, chise::chise_ds_get_feature($chise_ds, chise::get_uchar($_)));
   }
   $chise_newid = 0x0F0000;
 }
@@ -30,19 +30,19 @@ sub get_feature_value{ # feature, char_id -> 0 or value
   if(scalar(@_) != 2){
     return 0;
   }
-  my $ft = chise::chise_ds_get_feature($chise_ds, chise_tools::get_uchar($_[0]));
-  chise_tools::clear_buffer;
+  my $ft = chise::chise_ds_get_feature($chise_ds, chise::get_uchar($_[0]));
+  chise::clear_buffer;
   chise::chise_char_gets_feature_value($_[1], $ft,
-                                       $chise_tools::buffer, $chise_tools::buffer_size);
-  return chise_tools::get_char($chise_tools::buffer);
+                                       $chise::buffer, $chise::buffer_size);
+  return chise::get_char($chise::buffer);
 }
 
 sub set_feature_value{ # feature, char_id, value -> result
   if(scalar(@_) != 3){
     return 0;
   }
-  my $ft = chise::chise_ds_get_feature($chise_ds, chise_tools::get_uchar($_[0]));
-  chise::chise_char_set_feature_value($_[1], $ft, chise_tools::get_uchar($_[2]));
+  my $ft = chise::chise_ds_get_feature($chise_ds, chise::get_uchar($_[0]));
+  chise::chise_char_set_feature_value($_[1], $ft, chise::get_uchar($_[2]));
   return 1;
 }
 
@@ -61,9 +61,9 @@ sub search_newid{ # search new char_id and update $chise_newid
   while(1){
     my $found = 0;
     foreach(@chise_ft){
-      chise_tools::clear_buffer;
-      chise::chise_char_gets_feature_value($chise_newid, $_, $chise_tools::buffer, $chise_tools::buffer_size);
-      if(length(chise_tools::get_char($chise_tools::buffer)) != 0){
+      chise::clear_buffer;
+      chise::chise_char_gets_feature_value($chise_newid, $_, $chise::buffer, $chise::buffer_size);
+      if(length(chise::get_char($chise::buffer)) != 0){
         $found = 1;
         last;
       }
@@ -85,7 +85,7 @@ sub decode_char{ # feature, int -> charID, 0
   if(scalar(@_) != 2){
     return 0;
   }
-  return chise::chise_ds_decode_char($chise_ds, chise_tools::get_uchar($_[0]), $_[1]);
+  return chise::chise_ds_decode_char($chise_ds, chise::get_uchar($_[0]), $_[1]);
 }
 
 1;
