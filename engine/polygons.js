89391b0cb321beedeefb3b879599089fb1188bd7
Koichi KAMICHI <kamichi@fonts.jp>
2009-02-19 07:09:25 +0000

    Added a function to clear data of polygons array.

--- engine/polygons.js
+++ engine/polygons.js
@@ -1,5 +1,10 @@
 function Polygons(){
   // method
+ 	function clear(){ // void
+    this.array = new Array();
+  }
+  Polygons.prototype.clear = clear;
+	
   function push(polygon){ // void
     // only a simple check
     var minx = 200;
