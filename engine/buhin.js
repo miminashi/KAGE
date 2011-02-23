8ec9d61c13e5ad147779fb84828f31cbbdbbbab2
Koichi KAMICHI <kamichi@fonts.jp>
2011-02-23 05:30:56 +0000

    Use hash instead of array.

--- engine/buhin.js
+++ engine/buhin.js
@@ -1,38 +1,21 @@
 function Buhin(number){
   // method
-  function push(name, data){ // void
-    var temp = new Object();
-    temp.name = name;
-    temp.data = data;
-    this.array.push(temp);
-  }
-  Buhin.prototype.push = push;
-  
   function set(name, data){ // void
-    var temp = new Object();
-    temp.name = name;
-    temp.data = data;
-    for(var i = 0; i < this.array.length; i++){
-      if(this.array[i].name == name){
-        this.array.splice(i, 1);
-      }
-    }
-    this.array.push(temp);
+    this.hash[name] = data;
   }
+  Buhin.prototype.push = set;
   Buhin.prototype.set = set;
   
   function search(name){ // string
-    for(var i = 0; i < this.array.length; i++){
-      if(this.array[i].name == name){
-        return this.array[i].data;
-      }
+    if(this.hash[name]){
+      return this.hash[name];
     }
     return ""; // no data
   }
   Buhin.prototype.search = search;
   
   // property
-  this.array = new Array();
+  this.hash = {};
   
   // initialize
   // no operation
